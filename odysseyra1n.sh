9fe00dab2ceffd062a5a91f7f31b95707fee2365
gamono23 <alpatovaroslav3@gmail.com>
2023-02-25 21:50:29 +0300

    BRO WHY SED NO WORK

--- odysseyra1n.sh
+++ odysseyra1n.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Variables down there:
 os=$(uname)
-if [ "$os" == 'Darwin']; # This one checks architecture of your CPU
+if [ "$os" == 'Darwin' ]; # This one checks architecture of your CPU
     then
         un=$(uname -m)
             if [ "$un" == 'x86_64' ];
@@ -15,6 +15,7 @@ if [ "$os" == 'Darwin']; # This one checks architecture of your CPU
                 if [ "$un" == 'armv*' ];
                 then
                 arch=arm
+                fi
     else
             un=$(uname -m)
             if [ "$un" == 'x86_64' ];
@@ -28,10 +29,10 @@ if [ "$os" == 'Darwin']; # This one checks architecture of your CPU
                 if [ "$un" == 'armv*' ];
                 then
                 arch=arm
+                fi
 fi
 deviceconnection=$(ideviceinfo -s) | grep ERROR 
-deviceversion=$(ideviceinfo -k ProductVersion)
-sedversion=echo $deviceversion | sed -r 's/\./,/g'
+sedversion=echo $(ideviceinfo -k ProductVersion) | sed -r 's/\./,/g'
 # Checks(main)
 if [[ -z {$deviceconnection+ERROR} ]];
 then
@@ -46,10 +47,15 @@ else
         clear
         echo "Hello, "$(whoami) "on" $(ideviceinfo -k ProductVersion)"!"
         # Download checkra1n and procursus bootstrap
+        echo "Downloading required packages!"
         mkdir ./work
         if [ "$arch" == 64bit];
-        then
-        wget https://assets.checkra.in/downloads/linux/cli/x86_64/dac9968939ea6e6bfbdedeb41d7e2579c4711dc2c5083f91dced66ca397dc51d/checkra1n
-        wget 
+            then
+                wget https://assets.checkra.in/downloads/linux/cli/x86_64/dac9968939ea6e6bfbdedeb41d7e2579c4711dc2c5083f91dced66ca397dc51d/checkra1n
+                wget https://raw.githubusercontent.com/coolstar/Odyssey-bootstrap/master/procursus-deploy-linux-macos.sh
+            else
+                wget https://assets.checkra.in/downloads/linux/cli/arm64/43019a573ab1c866fe88edb1f2dd5bb38b0caf135533ee0d6e3ed720256b89d0/checkra1n
+                wget https://raw.githubusercontent.com/coolstar/Odyssey-bootstrap/master/procursus-deploy-linux-macos.sh
+        fi
     fi    
 fi
