838f4abc58032735b1d039e3dff9c7e8feea283a
gamono23 <alpatovaroslav3@gmail.com>
2023-02-25 19:22:10 +0300

    First commit

---
+++ odysseyra1n.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Hello, "$(whoami) "on" $(ideviceinfo -k ProductVersion)"!"
+sleep 2
+clear
+sleep 1
+echo "Getting device information."
+sleep 1
+clear
+echo "Getting device information.."
+sleep 1
+clear
+echo "Getting device information..."
