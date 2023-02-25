cb9d755014a8b9c7f44ed4ac990e03ea8cd90e90
gamono23 <alpatovaroslav3@gmail.com>
2023-02-25 20:07:42 +0300

    ifelseifelseifelseifelseifelseifelseifelse

--- odysseyra1n.sh
+++ odysseyra1n.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+deviceconnection=$(ideviceinfo -s) | grep ERROR 
+if [[ -z {$deviceconnection+ERROR} ]];
+then
+echo "Connect device or check USB."
+else
+echo "Starting..."
+sleep 1
+clear
 echo "Hello, "$(whoami) "on" $(ideviceinfo -k ProductVersion)"!"
 sleep 2
-clear
 sleep 1
 echo "Getting device information."
 sleep 1
@@ -10,3 +17,4 @@ echo "Getting device information.."
 sleep 1
 clear
 echo "Getting device information..."
+fi
