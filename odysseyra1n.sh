7df1469bbbacd0b9d3b649996233e4520b135b04
gamono23 <alpatovaroslav3@gmail.com>
2023-02-26 15:23:30 +0300

    remove versions and other things.

--- odysseyra1n.sh
+++ odysseyra1n.sh
@@ -31,31 +31,31 @@ if [ "$os" == 'Darwin' ]; # This one checks architecture of your CPU
                 arch=arm
                 fi
 fi
-deviceconnection=$(ideviceinfo -s) | grep ERROR 
-sedversion=echo $(ideviceinfo -k ProductVersion) | sed -r 's/\./,/g'
+deviceconnection=$(ideviceinfo -s) | grep ERROR
+# sedversion=echo $(ideviceinfo -k ProductVersion) | sed -r 's/\./,/g'
+# sedversion=14 # for testing, remove before somehting like release
 # Checks(main)
 if [[ -z {$deviceconnection+ERROR} ]];
 then
     echo "Connect device or check USB."
 else
-    if [[ $sedversion -lt 15 ]]; 
-        then
-            echo "Your version is too new for this!"
-        else
-            echo "Continuing..."
+   # if [[ $sedversion -lt 12 ]]; 
+       # then
+        #    echo "Your version is too new for this!"
+       # else
+        #    echo "Continuing..."
         sleep 1
         clear
         echo "Hello, "$(whoami) "on" $(ideviceinfo -k ProductVersion)"!"
         # Download checkra1n and procursus bootstrap
         echo "Downloading required packages!"
         mkdir ./work
-        if [ "$arch" == 64bit];
-            then
-                wget https://assets.checkra.in/downloads/linux/cli/x86_64/dac9968939ea6e6bfbdedeb41d7e2579c4711dc2c5083f91dced66ca397dc51d/checkra1n
-                wget https://raw.githubusercontent.com/coolstar/Odyssey-bootstrap/master/procursus-deploy-linux-macos.sh
-            else
-                wget https://assets.checkra.in/downloads/linux/cli/arm64/43019a573ab1c866fe88edb1f2dd5bb38b0caf135533ee0d6e3ed720256b89d0/checkra1n
-                wget https://raw.githubusercontent.com/coolstar/Odyssey-bootstrap/master/procursus-deploy-linux-macos.sh
-        fi
-    fi    
+         # No more x32 or arm64, mayber later
+                wget https://assets.checkra.in/downloads/linux/cli/x86_64/dac9968939ea6e6bfbdedeb41d7e2579c4711dc2c5083f91dced66ca397dc51d/checkra1n -P ./work/
+                wget https://raw.githubusercontent.com/coolstar/Odyssey-bootstrap/master/procursus-deploy-linux-macos.sh -P ./work/
+        cd work
+        chmod +x checkra1n # Hope it doesn't breaks anything # Ok it does...
+        chmod +x procursus.sh
+        ./checkra1n -c # -c is for cya
+    # fi    
 fi
